1730579f821e25a134bd0a2b12ee5e47c3e2c8fa
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2026-02-20 14:36:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const cartRoutes    = require('./routes/cartRoute');
 const orderRoutes   = require('./routes/orderRoute');
 const chatRoutes    = require('./routes/chatRoute');
 
+
 const { startAbandonedCartJob } = require('./jobs/abandonedCartEmail');
 const { initSocket }            = require('./socket');
 
@@ -50,4 +51,5 @@ app.set('io', io);
 // ✅ CAMBIO CLAVE: usar el puerto dinámico
 const PORT = process.env.PORT || 5000;
 httpServer.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
 
