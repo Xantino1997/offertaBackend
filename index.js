3327db66f72d726b9ed9dd0183440a3b60bf0977
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2026-02-20 14:35:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin:  'https://offerta-six.vercel.app/' || 'http://localhost:3000',
   credentials: true,
 }));
 app.use(express.json());
@@ -50,3 +50,4 @@ app.set('io', io);
 // ✅ CAMBIO CLAVE: usar el puerto dinámico
 const PORT = process.env.PORT || 5000;
 httpServer.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
